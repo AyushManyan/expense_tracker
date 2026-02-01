7d1ac8c2b524cf23b75995b67ec80e1d6e016bce
AyushManyan <ayushmanyan2003@gmail.com>
2026-02-01 11:39:06 +0530

    deploy

--- backend/server.js
+++ backend/server.js
@@ -43,3 +43,5 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+module.exports = app;
--- frontend/expense-tracker/src/utils/apiPath.js
+++ frontend/expense-tracker/src/utils/apiPath.js
@@ -1,4 +1,5 @@
-export const BASE_URL = "http://localhost:8000/";
+// export const BASE_URL = "http://localhost:8000/";
+export const BASE_URL = "https://spensight.vercel.app/";
 
 
 //  utils/apiPath.js
